3bba14e96ddcc66db9a605d730c2772260916811
SantaCruz77 <steveyamada66@gmail.com>
2023-01-15 11:27:13 +0900

    Completion of template

--- index.php
+++ index.php
@@ -1,15 +1,41 @@
-<?php echo "Hello PHP";
+<?php
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP掲示板</title>
+	<meta charset="UTF-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 	<title>PHP掲示板</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-  
+	<h1 class="title">PHPで掲示板アプリ</h1>
+	<hr>
+	<div class="boardWrapper">
+		<section>
+			<article>
+				<div class="wrapper">
+					<div class="nameArea">
+						<span>名前:</span>
+						<p class="username">shinCode</p>
+						<time>:2023/1/15</time>
+					</div>
+					<p class="comment">手書きコメントです。</p>
+				</div>
+			</article>
+		</section>
+		<form class="formWrapper">
+			<div>
+				<input type="submit" value="書き込む">
+				<label for="">名前:</label>
+				<input type="text" name="username!">
+			</div>
+			<div>
+				<textarea class="commentTextArea"></textarea>
+			</div>
+		</form>
+	</div>
 </body>
 </html>
